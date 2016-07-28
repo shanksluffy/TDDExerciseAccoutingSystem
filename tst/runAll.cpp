a5e0a153e17da825a2653d44166bb5d5201589c9
shanksluffy <wtopmone@gmail.com>
2016-07-28 21:06:46 +0800

    add test case for part1
    add test case for part1

--- tst/runAll.cpp
+++ tst/runAll.cpp
@@ -1,5 +1,5 @@
 #include <gtest/gtest.h>
-
+#include "MonthPhoneBill.hpp"
 
 class TestGame : public ::testing::Test{
 public:
@@ -12,11 +12,46 @@ public:
   }
 };
 
-TEST(TestGame, startFromHere)
+/*TEST(TestGame, startFromHere)
 {
   FAIL()<<"start from here!";
+}*/
+
+TEST(TestGame, ChooseGolgenWithOneLine)
+{
+  EPLAN_TYPE plantype = EPLAN_TYPE_GOLDEN;
+  u32 linnum = 1;
+  MonthPhoneBill mBill(plantype, linnum);
+  double monthbill = mBill.GetMonthBill();
+  EXPECT_EQ(49.95, monthbill);
 }
 
+TEST(TestGame, ChooseGolgenWithTwoLine)
+{
+  EPLAN_TYPE plantype = EPLAN_TYPE_GOLDEN;
+  u32 linnum = 2;
+  MonthPhoneBill mBill(plantype, linnum);
+  double monthbill = mBill.GetMonthBill();
+  EXPECT_EQ(49.95 + 14.50, monthbill);
+}
+
+TEST(TestGame, ChooseSilverWithOneLine)
+{
+  EPLAN_TYPE plantype = EPLAN_TYPE_SILVER;
+  u32 linnum = 1;
+  MonthPhoneBill mBill(plantype, linnum);
+  double monthbill = mBill.GetMonthBill();
+  EXPECT_EQ(29.95, monthbill);
+}
+
+TEST(TestGame, ChooseGolgenWithThreeLine)
+{
+  EPLAN_TYPE plantype = EPLAN_TYPE_SILVER;
+  u32 linnum = 3;
+  MonthPhoneBill mBill(plantype, linnum);
+  double monthbill = mBill.GetMonthBill();
+  EXPECT_EQ(29.95 + 21.50*2, monthbill);
+}
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
